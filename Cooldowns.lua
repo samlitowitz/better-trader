3162159750cc3754ddd0ed35eece700254edcfb2
Sam L <samlitowitz@users.noreply.github.com>
2023-09-02 07:36:52 -0400

    Add some cooldowns for testing

--- Cooldowns.lua
+++ Cooldowns.lua
@@ -2,6 +2,27 @@ local addonName, addon = ...
 
 -- [spelldID] = {isOffensive, isDefensive, duration, charges, class, cooldown}
 
+
+
 addon.Cooldowns = {
 	-- Death Knight
+	-- Blood
+	-- Frost
+	-- Icebound Fortitude
+	[48792] = {
+		isDefensive = true,
+		isOffensive = false,
+		classID = addon.ClassIDs.DEATH_KNIGHT,
+		specID = addon.SpecIDs.FROST,
+		damageType = addon.DamageTypes.ALL,
+	},
+	-- Pillar of Frost
+	[51271] = {
+		isDefensive = false,
+		isOffensive = true,
+		classID = addon.ClassIDs.DEATH_KNIGHT,
+		specID = addon.SpecIDs.FROST,
+		damageType = Bit.bit32.bor(addon.DamageTypes.PHYSICAL, addon.DamageTypes.FROST),
+	}
+	-- Unholy
 }
